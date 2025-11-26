e146f75d9234fa55a3dbeda31cdf22370a412604
Michael Schwarz <michael_schwarz@me.com>
2025-11-26 22:22:52 +0100

    Add call to billing info

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+set -a
+source .env
+set +a
+
 docker build -t jupyter-image-ml0 .
-docker run -p 8888:8888 -v .:/home/jovyan/work jupyter-image-ml0
+docker run -p 8888:8888 -e X_API_KEY="${X_API_KEY}" -v .:/home/jovyan/work jupyter-image-ml0
