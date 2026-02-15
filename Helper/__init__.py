a227406b31ff526515f25ca36409678240afd199
Michael Schwarz <michael_schwarz@me.com>
2026-02-15 08:00:57 +0100

    Cleanup project an analysis a bit

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,7 +1,5 @@
 from .stakingrewards_api import StakingRewardsAPIClient
-from .rated_api import RatedAPIClient
 
 __all__ = [
-    "StakingRewardsAPIClient",
-    "RatedAPIClient",
+    "StakingRewardsAPIClient"
 ]
